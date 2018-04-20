defa0b8b7f14cfba0fe8eb5fef4bba5356e8f1b4
Eduen Sarceño <s.aeduen@gmail.com>
2018-04-20 03:12:59 -0600

    Commit inicial

---
+++ src/core/TmoClient.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace YuriDb.TMO
+{
+    public class TmoPage
+    {
+
+        public Uri BaseUri { get; private set; } = null;
+        public JObject Data { get; private set; } = null;
+        public uint RateLimit { get; private set; } = 0;
+        public uint Remaining { get; private set; } = 0;
+
+        public TmoPage(Uri baseUri, JObject data, uint rateLimit, uint remaining)
+        {
+            if (data == null || baseUri == null) { 
+                throw new ArgumentNullException();
+            }
+            BaseUri = baseUri;
+            Data = data;
+            RateLimit = rateLimit;
+            Remaining = remaining;
+        }
+
+        public bool HasNext()
+        {
+            return (UInt32) Data["last_page"] > (UInt32) Data["current_page"];
+        }
+
+        public uint NextPage()
+        {
+            return (UInt32) Data["current_page"] + 1;
+        }
+
+        public void InheritRateLimit(TmoPage page)
+        {
+            RateLimit = page.RateLimit;
+            Remaining = page.Remaining;
+        }
+    }
+
+    public class TmoClient
+    {
+        public HttpClient Client { get; private set; }
+
+        public TmoClient()
+        {
+            Client = new HttpClient();
+            var headers = Client.DefaultRequestHeaders;
+            headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            headers.Accept.Add(new MediaTypeWithQualityHeaderValue("text/plain"));
+            headers.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+            headers.Connection.Add("keep-alive");
+            headers.Add("Cache-mode", "no-cache");
+            headers.Add("X-Requested-With", "XMLHttpRequest");
+            headers.Referrer = TmoUri;
+            headers.Host = "www.tumangaonline.com";
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+        }
+
+        public TmoPage GetPagina(Uri baseUri, uint page, uint itemsPerPage)
+        {
+            UriBuilder ub = new UriBuilder(baseUri);
+            ub.Query = ub.Query.Substring(1) + $"&page={page}&itemsPerPage={itemsPerPage}";
+            HttpResponseMessage response = Client.GetAsync(ub.Uri).GetAwaiter().GetResult();
+            string rateLimit = null;
+            string remaining = null;
+            {
+                IEnumerator<string> i = response.Headers.GetValues("x-ratelimit-limit").GetEnumerator();
+                i.MoveNext();
+                rateLimit = i.Current;
+
+                i = response.Headers.GetValues("x-ratelimit-remaining").GetEnumerator();
+                i.MoveNext();
+                remaining = i.Current;
+            }
+
+            string content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            JObject data;
+            try {
+                data = JObject.Parse(content);
+            } catch (JsonReaderException e) {
+                throw new Exception("Salida extraña del servidor, contenido: \n" + content, e);
+            }
+
+            TmoPage result = new TmoPage(
+                baseUri,
+                data,
+                UInt32.Parse(rateLimit),
+                UInt32.Parse(remaining)
+            );
+            response.Dispose();
+            return result;
+        }
+
+        public TmoPage GetPagina(Uri baseUri, uint page)
+        {
+            return GetPagina(baseUri, page, 25);
+        }
+
+        public int EncontrarManga(MangaYuri manga)
+        {
+            TmoPage cache = GetPagina(MangasYuri, 1, 1);
+
+            uint inf = 0;
+            uint sup = (UInt32) cache.Data["total"] - 1;
+
+            int n = 0;
+            int max = (int) Math.Ceiling((decimal) (Math.Log((double) sup) / Math.Log(2d)));
+
+            while (n < max) { 
+                uint p = (inf + sup) / 2;
+                if (cache.Remaining == 0) {
+                    Thread.Sleep(60 * 1000);
+                }
+                Console.WriteLine("Búscando en intervalo [{0}, {1}], actual = {2}", inf, sup, p);
+                cache = GetPagina(MangasYuri, p + 1, 1);
+                JObject obj = (JObject) (((JArray) cache.Data["data"])[0]);
+                JObject info = (JObject) obj["info"];
+                DateTime date = DateTime.Parse((string) info["fechaCreacion"]);
+
+                if (manga.TmoCreacion.CompareTo(date) > 0) {
+                    inf = p;
+                } else if (manga.TmoCreacion.CompareTo(date) < 0) {
+                    sup = p;
+                } else {
+                    return (int) p;
+                }
+                n++;
+            }
+            Console.WriteLine("Loop infinito detectado");
+            return -1;
+        }
+
+        public static readonly Uri TmoUri = new Uri("https://www.tumangaonline.com");
+        public static readonly Uri MangasYuri = new Uri("https://www.tumangaonline.com/api/v1/mangas?generos=[17]&sortDir=asc&sortedBy=fechaCreacion");
+
+        public static Uri UriManga(uint tmoId)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("https://www.tumangaonline.com/api/v1/mangas/");
+            sb.Append(tmoId);
+            sb.Append("/capitulos?tomo=-1");
+            return new Uri(sb.ToString());
+        }
+
+       
+    }
+}
---
+++ src/core/YuriDb.cs
@@ -0,0 +1,185 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace YuriDb
+{
+    public enum MangaEstado : byte
+    {
+        EnEmision,
+        Finalizado
+    }
+
+    public enum MangaTipo : byte
+    {
+        Manga,
+        Manhua,
+        Manhwa,
+        Novela,
+        Propio,
+        Otro
+    }
+
+    public class MangaYuri
+    {
+        public uint Id { get; private set; }
+        public uint TmoId { get; private set; }
+        public DateTime TmoCreacion { get; private set; } = DateTime.Now;
+        public uint Capitulos { get; set; }
+        public Uri Imagen { get; set; } 
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public MangaEstado Estado { get; set; }
+        public MangaTipo Tipo { get; set; }
+
+        public MangaYuri(uint id, uint tmoId, DateTime tmoCreacion)
+        {
+            Id = id;
+            TmoId = tmoId;
+            TmoCreacion = tmoCreacion;
+            Capitulos = 0;
+        }
+
+        public MangaYuri(uint tmoId, DateTime tmoCreacion) : 
+            this(0, tmoId, tmoCreacion)
+        {
+        }
+
+    }
+
+    public class YuriDb
+    {
+
+        public bool Connected { get; private set; } 
+        public string Password { get; set; }
+        public string Host { get; set; }
+        public string User { get; set; }
+        public string Db   { get; set; }
+        public int Port    { get; set; }
+        public MySqlConnection Connection { get; private set; }
+
+        public void CreateConnection()
+        {
+            if (Connection == null) {
+                Connection = new MySqlConnection($"SERVER={Host};PORT={Port};UID={User};PASSWORD={Password}");
+            }
+        }
+
+        public void OpenConnection()
+        {
+            if (!Connected) {
+                Connection.Open();
+                Connected = true;
+            }
+        }
+
+        public void Close()
+        {
+            if (Connection == null)
+                return;
+            if (Connected) 
+                Connection.Close();
+        }
+
+        public void Dispose()
+        {
+            if (Connection == null)
+                return;
+            Connection.Dispose();
+            Connection = null;
+        }
+
+        public void CrearBaseDeDatos()
+        {
+            MySqlCommand cmd = Connection.CreateCommand();
+            cmd.CommandText = $"CREATE DATABASE IF NOT EXISTS `{Db}` CHARACTER SET = 'utf8'";
+            cmd.ExecuteNonQuery();
+        }
+
+        public void CrearTablas()
+        {
+            MySqlCommand cmd = Connection.CreateCommand();
+            cmd.CommandText = 
+            string.Format(
+@"CREATE TABLE IF NOT EXISTS `{0}`.`mangas` (
+id              INT UNSIGNED        AUTO_INCREMENT PRIMARY KEY,
+tmoId           INT UNSIGNED        NOT NULL UNIQUE KEY,
+tmoCreación     DATE                NOT NULL COMMENT 'Se usa internamente para búsqueda binaria',
+nombre          VARCHAR(512)        NOT NULL,
+imagen          VARCHAR(256)        NULL,
+descripción     VARCHAR(1024)       NULL,
+capítulos       INT UNSIGNED        NOT NULL DEFAULT 0,
+estado          TINYINT UNSIGNED    NOT NULL DEFAULT 0,
+tipo            TINYINT UNSIGNED    NOT NULL DEFAULT 0,
+FULLTEXT INDEX (nombre)
+) ENGINE = MyIsam, COMMENT = 'Guarda referencias y la información de los mangas extraídos de TMO'",
+            Db);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void AgregarManga(MangaYuri manga)
+        {
+            if (manga == null) {
+                throw new ArgumentNullException("manga es nulo");
+            }
+
+            if (manga.TmoId < 0) {
+                throw new ArgumentException("tmoId no puede ser negativo");
+            }
+
+            if (manga.Nombre == null || manga.Nombre.Length == 0) {
+                throw new ArgumentException("el nombre es nulo o vacío");
+            }
+
+
+            MySqlCommand cmd = Connection.CreateCommand();
+            cmd.CommandText = 
+            string.Format(
+ @"INSERT INTO `{0}`.`mangas` (
+    tmoId, tmoCreación, nombre, descripción, capítulos, imagen, tipo, estado
+) values (
+    @tmoId, @tmoCreación, @nombre, @descripción, @capítulos, @imagen, @tipo, @estado
+)", 
+            Db);
+            cmd.Parameters.AddWithValue("@tmoId", manga.TmoId);
+            cmd.Parameters.AddWithValue("@tmoCreación", manga.TmoCreacion.Date.ToString("yyyy-MM-dd"));
+            cmd.Parameters.AddWithValue("@nombre", manga.Nombre);
+            cmd.Parameters.AddWithValue("@descripción", manga.Descripcion ?? string.Empty);
+            cmd.Parameters.AddWithValue("@capítulos", manga.Capitulos);
+            cmd.Parameters.AddWithValue("@imagen", manga.Imagen == null ? "NULL" : manga.Imagen.ToString());
+            cmd.Parameters.AddWithValue("@tipo", manga.Tipo);
+            cmd.Parameters.AddWithValue("@estado", manga.Estado);
+            cmd.Prepare();
+            cmd.ExecuteNonQuery();
+        }
+
+        public long GetCantidadMangas()
+        {
+            MySqlCommand cmd = Connection.CreateCommand();
+            cmd.CommandText = $"SELECT COUNT(*) FROM `{Db}`.`mangas`";
+            return (Int64) cmd.ExecuteScalar();
+        }
+
+        public MangaYuri GetUltimaAgregacion()
+        {
+            MySqlCommand cmd = Connection.CreateCommand();
+            cmd.CommandText = $"SELECT * FROM `{Db}`.`mangas` ORDER BY `tmoCreación` DESC LIMIT 1";
+            MySqlDataReader reader = cmd.ExecuteReader();
+            reader.Read();
+            MangaYuri manga = new MangaYuri(
+                (UInt32) reader[0],
+                (UInt32) reader[1],
+                (DateTime) reader[2]
+            ) {
+                Nombre = (string) reader[3],
+                Imagen = new Uri((string) reader[4]),
+                Descripcion = (string) reader[5],
+                Capitulos = (UInt32) reader[6],
+                Estado = (MangaEstado) reader[7],
+                Tipo = (MangaTipo) reader[8]
+            };
+            reader.Close();
+            return manga;
+        }
+
+    }
+}
